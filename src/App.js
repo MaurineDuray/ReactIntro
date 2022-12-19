e27b2b4bc6fdc427f71fba52472cab101362e699
MaurineDuray <maurine.duray@skynet.be>
2022-12-19 09:47:03 +0100

    augmentation pour chaque et vidage de l'input

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ const league = {
     nom: "Catwoman",
     age: 31
   },
+  membre5 :{
+    nom: "Napoléon",
+    age: 4
+  }
 }
 
 class App extends Component {
@@ -40,12 +44,12 @@ class App extends Component {
   componentWillUnmount(){
     console.log("démontage")
   }
-  handleClick = (nb) => {
+  handleClick = (id,nb) => {
     // modification du state
     // copier le state et modifier et puis setter
     const league = {...this.state.league} 
-    league.membre1.age += nb
-    this.setState({league})
+    league[id].age += nb
+    this.setState({league}) // si pas le même nom format league:league
   }
 
   handleShow =()=>{
@@ -53,6 +57,19 @@ class App extends Component {
     this.setState({isShow})
   }
 
+  handleChange = (event, id) =>{
+    const league = {...this.state.league} //on copie la valeur avant de la changer
+    const nom = event.target.value //récupère la valeur de l'input
+    league[id].nom = nom
+    this.setState({league:league})
+  }
+
+  hideName = (id)=>{
+    const league = {...this.state.league}
+    league[id].nom = "X"
+    league[id].age =0
+    this.setState({league})
+  }
   render() { 
     // return (
     //   // ici react et jsx
@@ -60,8 +77,18 @@ class App extends Component {
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, "React App"))
 
     const list = Object.keys(this.state.league).map(iteration=>{
+      //map boucle dans un tableau en reconstruisant un tableau avec le return
       return (
-        <Membre key={iteration} age={this.state.league[iteration].age} nom={this.state.league[iteration].nom} />
+        <Membre 
+        key={iteration} 
+        age={this.state.league[iteration].age} 
+        nom={this.state.league[iteration].nom}
+        handleChange= {(event)=>{this.handleChange(event,iteration)}} 
+        hideName = {()=>this.hideName(iteration)}
+        plus = {this.state.plus}
+        handleClick = {()=>{this.handleClick(iteration,this.state.plus)}}/>
+        //key = clé unique nécessaire pour React (voir recommandation dans la console grâce au React StrictMode)
+        
       )
     })
     return (
@@ -72,22 +99,9 @@ class App extends Component {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nesciunt minima assumenda ratione, voluptatum, quod labore distinctio, repudiandae mollitia ex aspernatur? Voluptatem ipsam similique dolores reprehenderit consectetur quae accusantium aliquam unde hic harum culpa minus ex eveniet, eos dolor ipsa quia numquam. Expedita ipsum odio debitis velit explicabo quas ad illo ex numquam nemo, error quam accusamus possimus est dolorum? Aut distinctio id cupiditate pariatur, impedit voluptatum? Dolorem quas ex accusamus libero fugit aspernatur, nihil veniam unde nam qui id magnam explicabo non voluptatum, rem quis. Sed exercitationem at ducimus dolor, inventore, quos nisi asperiores vel repellendus, dolorem autem earum.</p>
           {this.state.league.membre1.nom}
          
-          {list}
-          <Membre
-            age={4}
-            nom="Napoléon"
-          >
-            {this.state.isShow ? <strong>Je suis l'empereur! </strong> :null}
-              <button onClick={this.handleShow}>
-                {this.state.isShow? 'Cacher' : 'Montrer'}
-              </button>
-          </Membre>
+          {list} 
+          {/* appel du tableau et il le parcours lui même */}
           
-          <Button 
-            plus = {this.state.plus}
-            vieillir = {()=>this.handleClick(this.state.plus)}
-           
-          />
          
         </div>
         
